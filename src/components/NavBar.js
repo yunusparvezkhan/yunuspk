efb17b5dac33c63245fbe7031433393892fdf5db
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-20 23:44:15 +0530

    feat: create NavBar template

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NavBar</h1>
+        <div className='w-full bg-blue-500 h-14 flex flex-row items-center justify-between text-white' >
+            <h1 className='text-3xl font-thin ml-10'>YunusPK</h1>
+            <div className='flex flex-row items-center gap-5'>
+                <button>home</button>
+                <button>about</button>
+                <button>blogs</button>
+                <button>projects</button>
+                <button>achievements</button>
+                <button>academics</button>
+                <button>playlists</button>
+                <button>contact</button>
+            </div>
+            <div className='mr-10 bg-gray-800 h-9 flex flex-row items-center p-2' >
+                <input className='bg-transparent border-none outline-none' placeholder='Search...' />
+            </div>
         </div>
     )
 }
