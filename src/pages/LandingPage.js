4839526424977aabbe51e5534895991c9e9ec08b
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-21 23:59:45 +0530

    style: change heights

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './styles/landingpage.css';
+import ypkWelcome from '../media/photos/ypk-welcome.png';
+import introSecFooter from './elements/intro-sec-footer.svg'
 
 const LandingPage = () => {
 	return (
-		<div>
+		<div className='landing-page' >
 			<div className='sec-intro'>
 				<div className='sec-intro-container text-white'>
 					<h4>Hello.Namaste</h4>
@@ -11,7 +13,13 @@ const LandingPage = () => {
 					<h1>Yunus</h1>
 					<h3>Parvez</h3>
 					<h2>Khan</h2>
+					<img
+						alt="Yunus Parvez Khan smilling and introducing something"
+						src={ypkWelcome}
+					/>
 				</div>
+				<img src={introSecFooter} className='intro-sec-footer' alt='Intro Section Footer Effect' />
+
 			</div>
 		</div>
 	);
