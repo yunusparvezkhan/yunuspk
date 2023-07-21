5591ab471a80ebcfcc177e8ad9c571df37bb8240
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-22 00:15:43 +0530

    style: reduced css file load using tailwindcss

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,8 +6,8 @@ import introSecFooter from './elements/intro-sec-footer.svg'
 const LandingPage = () => {
 	return (
 		<div className='landing-page' >
-			<div className='sec-intro'>
-				<div className='sec-intro-container text-white'>
+			<div className='sec-intro relative w-full'>
+				<div className='sec-intro-container text-white lftf t-0 absolute h-full w-1kpx'>
 					<h4>Hello.Namaste</h4>
 					<h5>I am</h5>
 					<h1>Yunus</h1>
@@ -16,9 +16,10 @@ const LandingPage = () => {
 					<img
 						alt="Yunus Parvez Khan smilling and introducing something"
 						src={ypkWelcome}
+						className='h-600px absolute t-100px right-0'
 					/>
 				</div>
-				<img src={introSecFooter} className='intro-sec-footer' alt='Intro Section Footer Effect' />
+				<img src={introSecFooter} className='intro-sec-footer absolute w-full' alt='Intro Section Footer Effect' />
 
 			</div>
 		</div>
