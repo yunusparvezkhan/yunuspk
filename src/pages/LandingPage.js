a7cddfed42e609f17fb7fdb3d8de5db7f10f08c1
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-22 22:15:11 +0530

    feat: make about section properties dynamic

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,11 +3,25 @@ import IntroSection from './sections/IntroSection';
 import AboutSection from './sections/AboutSection';
 import './styles/landingpage.css'
 
+import image from '../media/photos/ypk-2019-setup-selfie.png'
+
+
 const LandingPage = () => {
+
+	const para =
+		`
+			I am a Bharatiya scholar. I'm a Full Stack Web Developer, UI/UX Designer, YouTuber,
+			Filmmaker. I have an adventurous life and a lots of experiences. I love to
+			research things by digging deep onto the topics. My fields of expertise are
+			Web Development, Network Architecture, Computer Architecture, Blockchain,
+			Web - 3 etc. And I'm also pretty much into Physics, Psychology, Philosophy,
+			Geo - Politics, Economics, Socialism.
+		`
+
 	return (
 		<div className='landing-page text-white' >
 			<IntroSection />
-			<AboutSection />
+			<AboutSection image={image} alt="something" bgcolor="#182d4a" borderColor="#20EC94" paragraph={para} />
 		</div>
 	);
 }
--- src/pages/sections/AboutSection.js
+++ src/pages/sections/AboutSection.js
@@ -2,23 +2,17 @@ import React from 'react';
 import './styles/about-section.css';
 
 import HexagonImage from '../../components/HexagonImage';
-import image from '../../media/photos/ypk-2019-setup-selfie.png'
 
-const AboutSection = () => {
+const AboutSection = ({ image, alt, bgcolor, borderColor, paragraph }) => {
     return (
         <div className='about-section' >
             <div className='border w-3/4 h-600px relative left-1/2 ttf' >
                 <div className='relative left-1/2 top-1/2 ttff flex w-fit'>
-                    <HexagonImage image={image} alt="something" bgcolor="#182d4a" borderColor="#20EC94" />
+                    <HexagonImage image={image} alt={alt} bgcolor={bgcolor} borderColor={borderColor} />
                     <div className='flex flex-col' >
                         <h1 className='text-5xl ml-10 w-fit ' >a bit about me !</h1>
                         <p className='about-para ml-10 mt-10 text-lg' >
-                            I am a Bharatiya scholar. I'm a Full Stack Web Developer, UI/UX Designer, YouTuber,
-                            Filmmaker. I have an adventurous life and a lots of experiences. I love to
-                            research things by digging deep onto the topics. My fields of expertise are
-                            Web Development, Network Architecture, Computer Architecture, Blockchain,
-                            Web-3 etc. And I'm also pretty much into Physics, Psychology, Philosophy,
-                            Geo-Politics, Economics, Socialism.
+                            {paragraph}
                         </p>
                     </div>
                 </div>
