5c6c7fb8cf99e1ba567e324b897e2926b2b7a1d9
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-24 00:20:31 +0530

    feat: create blogs section template
    This commit creates a template for blogs section structure.

--- src/pages/LandingPage/sections/BlogsSection.js
+++ src/pages/LandingPage/sections/BlogsSection.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../styles/blogs-section.css'
 
 const BlogsSection = () => {
     return (
-        <div>blogsSection</div>
+        <div>
+            <div className='flex flex-col items-center justify-between blogs-sec-container'>
+                <h1 className='text-5xl w-fit mb-10' >Blogs</h1>
+                <div className='border mb-10 flex flex-row items-center justify-between gap-5  blogs-sec-cards-container'>
+                    <div className=' w-72 border h-80' >
+
+                    </div>
+                    <div className=' w-72 border h-80' >
+
+                    </div>
+                    <div className=' w-72 border h-80' >
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
