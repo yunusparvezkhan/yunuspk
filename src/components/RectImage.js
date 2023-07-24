1740dd5dcab40ed7e69c3d5b3830e3f6e2a6d5e1
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-25 00:07:22 +0530

    feat(RectImage): create automated sizing with a recieved aspect ratio
    This commit creates logics in RectImage that allows devs to provide any one
    data either width as 'w' prop or height as 'h' prop and a aspect ratio as
    'aspectRatio' prop and a prop 'dataType' to specify what data has been
    provided, to automatically size the another prop.

--- src/components/RectImage.js
+++ src/components/RectImage.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
-const RectImage = ({ image, alt, width, height }) => {
+//  The prop aspectRatio expects a array of two numbers being the aspect ratio of the frame
+//  w and h should be numbers and will be converted to whatever is passed in dataUnit
+const RectImage = ({ image, alt, width, height, dataType, dataUnit, w, h, aspectRatio }) => {
+    let finalWidth;
+    let finalHeight;
+
+    if (dataType === 'w') {
+        finalHeight = (w / aspectRatio[0]) * aspectRatio[1];
+    } else if (dataType === 'h') {
+        finalWidth = (h / aspectRatio[1]) * aspectRatio[0];
+    }
+
     return (
         <div>
             <div style={{
                 position: "relative",
-                width: `${width}`,
-                height: `${height}`,
+                width: `${width ? width : finalWidth + dataUnit}`,
+                height: `${height ? height : finalHeight + dataUnit}`,
                 clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)"
             }} >
                 <img
