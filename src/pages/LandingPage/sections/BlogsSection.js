d6bf3831fdd5f3e57cf921f8228e206c5f2b4889
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-25 23:37:23 +0530

    refactor(landing/blogs): fetch blogs data from a dataset to make it dynamic
    This commit will remove repeated code of blogs cards to one map function
    on a array of objects which contains the details of the blog cards as
    image, alt, title, subtitle props. This makes the code easy to refactor later
    to fetch data from a backend

--- src/pages/LandingPage/sections/BlogsSection.js
+++ src/pages/LandingPage/sections/BlogsSection.js
@@ -3,53 +3,53 @@ import '../styles/blogs-section.css'
 import RectImage from '../../../components/RectImage';
 
 const BlogsSection = () => {
+
+    const blogsData = [
+        {
+            image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+            alt: "something",
+            title: "Blog no 1",
+            subtitle: "25th July 2023 | Technology"
+        },
+        {
+            image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+            alt: "something",
+            title: "Blog no 2",
+            subtitle: "25th July 2023 | Technology"
+        },
+        {
+            image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
+            alt: "something",
+            title: "Blog no 3",
+            subtitle: "25th July 2023 | Technology"
+        }
+    ]
+
+    const renderBlogsGridCard = blogsData.slice(0, 3).map((blog) => {
+        return (
+            <div key={blog.title} className='flex flex-col justify-between cursor-pointer  blogs-section-card' >
+                <RectImage
+                    image={blog.image}
+                    alt={blog.alt}
+                    width={300}
+                    dataUnit='px'
+                    aspectRatio={[16, 9]}
+                    background="transparent"
+                />
+                <div className=' flex flex-col items-center  blogs-sec-card-content' >
+                    <label>{blog.title}</label>
+                    <span>{blog.subtitle}</span>
+                </div>
+            </div>
+        )
+    })
+
     return (
         <div>
             <div className='flex flex-col items-center justify-between blogs-sec-container'>
                 <h1 className='text-5xl w-fit mb-10' >Blogs</h1>
                 <div className='border mb-10 flex flex-row items-center justify-between gap-5  blogs-sec-cards-container'>
-                    <div className='flex flex-col justify-between  blogs-section-card' >
-                        <RectImage
-                            image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-                            alt="something"
-                            width={300}
-                            dataUnit='px'
-                            aspectRatio={[16, 9]}
-                            background="transparent"
-                        />
-                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
-                            <label>Blog Title</label>
-                            <span>ddth mm yyyy || Topic</span>
-                        </div>
-                    </div>
-                    <div className=' flex flex-col justify-between blogs-section-card' >
-                        <RectImage
-                            image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-                            alt="something"
-                            width={300}
-                            dataUnit='px'
-                            aspectRatio={[16, 9]}
-                            background="transparent"
-                        />
-                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
-                            <label>Blog Title</label>
-                            <span>ddth mm yyyy || Topic</span>
-                        </div>
-                    </div>
-                    <div className='flex flex-col justify-between blogs-section-card' >
-                        <RectImage
-                            image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-                            alt="something"
-                            width={300}
-                            dataUnit='px'
-                            aspectRatio={[16, 9]}
-                            background="transparent"
-                        />
-                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
-                            <label>Blog Title</label>
-                            <span>ddth mm yyyy || Topic</span>
-                        </div>
-                    </div>
+                    {renderBlogsGridCard}
                 </div>
             </div>
         </div>
