39054571658422e2248823911e1458bc00cf3ad4
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-25 23:59:20 +0530

    feat & style(landing/blogs): add link prop on blogsData objs
    This commit adds a link prop to each blog object and a anchor tag
    as a wrap to blog cards to redirect users to blogs. Also this commit
    adds some decent styles on hover to blogs cards

--- src/pages/LandingPage/sections/BlogsSection.js
+++ src/pages/LandingPage/sections/BlogsSection.js
@@ -9,38 +9,45 @@ const BlogsSection = () => {
             image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
             alt: "something",
             title: "Blog no 1",
-            subtitle: "25th July 2023 | Technology"
+            subtitle: "25th July 2023 | Technology",
+            link: 'https://github.com/ypkorg/Pushing_into_states_in_React'
         },
         {
             image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
             alt: "something",
             title: "Blog no 2",
-            subtitle: "25th July 2023 | Technology"
+            subtitle: "25th July 2023 | Technology",
+            link: 'https://github.com/ypkorg/slice-method-in-js'
         },
         {
             image: "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg",
             alt: "something",
             title: "Blog no 3",
-            subtitle: "25th July 2023 | Technology"
+            subtitle: "25th July 2023 | Technology",
+            link: 'https://github.com/ypkorg/delta-commits-convention'
         }
     ]
 
     const renderBlogsGridCard = blogsData.slice(0, 3).map((blog) => {
         return (
-            <div key={blog.title} className='flex flex-col justify-between cursor-pointer  blogs-section-card' >
-                <RectImage
-                    image={blog.image}
-                    alt={blog.alt}
-                    width={300}
-                    dataUnit='px'
-                    aspectRatio={[16, 9]}
-                    background="transparent"
-                />
-                <div className=' flex flex-col items-center  blogs-sec-card-content' >
-                    <label>{blog.title}</label>
-                    <span>{blog.subtitle}</span>
+            <a href={blog.link} rel='noreferrer' target='_blank' key={blog.title}>
+                <div className='flex flex-col justify-between cursor-pointer  blogs-section-card' >
+                    <div className='blogs-sec-card-rectimage'>
+                        <RectImage
+                            image={blog.image}
+                            alt={blog.alt}
+                            width={300}
+                            dataUnit='px'
+                            aspectRatio={[16, 9]}
+                            background="transparent"
+                        />
+                    </div>
+                    <div className=' flex flex-col items-center  blogs-sec-card-content' >
+                        <label>{blog.title}</label>
+                        <span>{blog.subtitle}</span>
+                    </div>
                 </div>
-            </div>
+            </a>
         )
     })
 
