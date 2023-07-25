9a1833135f44830ba7b8511f477812c229960655
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-25 22:03:40 +0530

    feat(landing/blogs): create blog title and details span below thumbnail

--- src/pages/LandingPage/sections/BlogsSection.js
+++ src/pages/LandingPage/sections/BlogsSection.js
@@ -8,7 +8,7 @@ const BlogsSection = () => {
             <div className='flex flex-col items-center justify-between blogs-sec-container'>
                 <h1 className='text-5xl w-fit mb-10' >Blogs</h1>
                 <div className='border mb-10 flex flex-row items-center justify-between gap-5  blogs-sec-cards-container'>
-                    <div className='' >
+                    <div className='flex flex-col justify-between  blogs-section-card' >
                         <RectImage
                             image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
                             alt="something"
@@ -17,12 +17,38 @@ const BlogsSection = () => {
                             aspectRatio={[16, 9]}
                             background="transparent"
                         />
+                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
+                            <label>Blog Title</label>
+                            <span>ddth mm yyyy || Topic</span>
+                        </div>
                     </div>
-                    <div className=' w-72 border h-80' >
-
+                    <div className=' flex flex-col justify-between blogs-section-card' >
+                        <RectImage
+                            image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+                            alt="something"
+                            width={300}
+                            dataUnit='px'
+                            aspectRatio={[16, 9]}
+                            background="transparent"
+                        />
+                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
+                            <label>Blog Title</label>
+                            <span>ddth mm yyyy || Topic</span>
+                        </div>
                     </div>
-                    <div className=' w-72 border h-80' >
-
+                    <div className='flex flex-col justify-between blogs-section-card' >
+                        <RectImage
+                            image="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+                            alt="something"
+                            width={300}
+                            dataUnit='px'
+                            aspectRatio={[16, 9]}
+                            background="transparent"
+                        />
+                        <div className=' flex flex-col items-center  blogs-sec-card-content' >
+                            <label>Blog Title</label>
+                            <span>ddth mm yyyy || Topic</span>
+                        </div>
                     </div>
                 </div>
             </div>
