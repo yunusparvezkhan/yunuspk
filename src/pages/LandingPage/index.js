a65ad5177386188d894a043078fc266c2bcaf3f9
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-27 00:38:12 +0530

    feat(landing/projects): create projects list & a preview panel

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -13,12 +13,41 @@ const LandingPage = () => {
 		return state.about;
 	})
 
+	const projectsData = [
+		{
+			id: "1",
+			title: "Delta__108_sigma_006",
+			subtitle: `Lorem Ipsum is a dummy text model that can be 
+			used in any kind of paragraph examples.Lorem Ipsum is a dummy text model that can be used in any kind of paragraph examples.`,
+			body: () => {
+				return (
+					<div>
+						<h2>Hello from project component 1</h2>
+					</div>
+				)
+			}
+		},
+		{
+			id: "2",
+			title: "rho_tifi",
+			subtitle: `Lorem Ipsum is a dummy text model that can be 
+			used in any kind of paragraph examples.Lorem Ipsum is a dummy text model that can be used in any kind of paragraph examples.`,
+			body: () => {
+				return (
+					<div>
+						<h2>Hello from project component 2</h2>
+					</div>
+				)
+			}
+		}
+	]
+
 	return (
 		<div className='landing-page text-white' >
 			<IntroSection />
 			<AboutSection image={images[4].link} alt="something" bgcolor="#182d4a" borderColor="#20EC94" paragraph={paragraph.text} />
 			<BlogsSection blogsData={blogsData} />
-			<ProjectsSection />
+			<ProjectsSection projectsData={projectsData} />
 		</div>
 	);
 }
--- src/pages/LandingPage/sections/ProjectsSection.js
+++ src/pages/LandingPage/sections/ProjectsSection.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/projects-section.css'
 
-const ProjectsSection = () => {
+// This component expects the `projectsData` prop to be an array of objects
+const ProjectsSection = ({ projectsData }) => {
+    const [activeProject, setActiveProject] = useState(0);
+
+    const renderProjectsList = projectsData.map((project, i) => {
+        return (
+            <div key={i} className='w-64 border' onClick={() => setActiveProject(i)} >
+                <h1 className='text-lg font-bold' >
+                    {project.title} <br></br>
+                </h1>
+                {project.subtitle}
+            </div>
+        )
+    })
+
+
     return (
         <div className='flex flex-col items-center  projects-sec' >
             <h1 className='text-5xl w-fit mb-20' >Projects</h1>
-            <div className='border  projects-sec-container' >
+            <div className='border  projects-sec-container flex flex-row' >
                 {/* Create a component for listing out the projects */}
+                <div className='flex flex-col cursor-pointer' >
+                    {renderProjectsList}
+                </div>
                 {/* Create a component for rendering a a function as recieved in a object */}
+                <div className='border'>
+                    {projectsData[activeProject].body()}
+                </div>
             </div>
         </div>
     )
