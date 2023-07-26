a2b83903f12b6c307b932cf4d8ac250771cf1db3
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-26 23:25:41 +0530

    refactor(landing/blogs): migrate blogsData to a separate file and pass through props

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -5,6 +5,7 @@ import AboutSection from './sections/AboutSection';
 import './styles/landingpage.css'
 import BlogsSection from './sections/BlogsSection';
 import ProjectsPage from '../ProjectsPage';
+import blogsData from './data/blogsData.json'
 
 const LandingPage = () => {
 
@@ -16,7 +17,7 @@ const LandingPage = () => {
 		<div className='landing-page text-white' >
 			<IntroSection />
 			<AboutSection image={images[4].link} alt="something" bgcolor="#182d4a" borderColor="#20EC94" paragraph={paragraph.text} />
-			<BlogsSection />
+			<BlogsSection blogsData={blogsData} />
 			<ProjectsPage />
 		</div>
 	);
--- src/pages/LandingPage/sections/BlogsSection.js
+++ src/pages/LandingPage/sections/BlogsSection.js
@@ -6,34 +6,7 @@ import Link from '../../../components/Link';
 import Button from '../../../components/Button';
 import { GiArmoredBoomerang } from 'react-icons/gi';
 
-const BlogsSection = () => {
-
-    const blogsData = [
-        {
-            image: "https://github.com/ypkorg/slice-method-in-js/raw/masterdoc/Slice%20method%202.png",
-            alt: "Slice Method in JS thumbnail",
-            title: "Slice Method in Javascript",
-            subtitle: "13th May 2023 | Javascript Programming",
-            thumbnailTitle: "Slice Method in JS",
-            link: 'https://github.com/ypkorg/slice-method-in-js'
-        },
-        {
-            image: "",
-            alt: "",
-            title: "Pushing into states in React",
-            subtitle: "11th May 2023 | ReactJS Programming",
-            thumbnailTitle: "Pushing into states in ReactJS",
-            link: 'https://github.com/ypkorg/Pushing_into_states_in_React'
-        },
-        {
-            image: "https://miro.medium.com/v2/resize:fit:720/0*v1o4-S6vNyVznnBY",
-            alt: "Jullian Assange",
-            title: "Imaginary Interview with the future of transparency, Julian Assange",
-            subtitle: "26th July 2023 | Cyber Security",
-            thumbnailTitle: "Cyber Security Do's & Don't's",
-            link: 'https://yunuspk.medium.com/imaginary-interview-with-the-future-of-transparency-julian-assange-714c5a251774'
-        }
-    ]
+const BlogsSection = ({ blogsData }) => {
 
     const renderBlogsGridCard = blogsData.slice(0, 3).map((blog) => {
         return (
