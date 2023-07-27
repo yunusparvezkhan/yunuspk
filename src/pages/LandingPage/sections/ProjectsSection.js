c9581e8bf9dbd699e7015ec6481ade455b450bcf
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-07-27 22:35:04 +0530

    style(landing/projects): style the list panel, container & preview panel

--- src/pages/LandingPage/sections/ProjectsSection.js
+++ src/pages/LandingPage/sections/ProjectsSection.js
@@ -9,11 +9,19 @@ const ProjectsSection = ({ projectsData }) => {
 
     const renderProjectsList = projectsData.map((project, i) => {
         return (
-            <div key={i} className='w-64 border' onClick={() => setActiveProject(i)} >
-                <h3 className='text-lg font-bold' >
-                    {project.title} <br></br>
+            <div key={i} className='projects-sec-card-container'
+                onClick={() => setActiveProject(i)}
+                style={{
+                    backgroundColor: activeProject === i ? "#252F39" : "transparent",
+                    width: "calc(100% + 1px)",
+                }}
+            >
+                <h3>
+                    {project.title}
                 </h3>
-                {project.subtitle}
+                <p>
+                    {project.subtitle.trim().slice(0, 85) + "..."}
+                </p>
             </div>
         )
     })
@@ -22,13 +30,13 @@ const ProjectsSection = ({ projectsData }) => {
     return (
         <div className='flex flex-col items-center  projects-sec' >
             <h1 className='text-5xl w-fit mb-20' >Projects</h1>
-            <div className='border  projects-sec-container flex flex-row' >
+            <div className='  projects-sec-container flex flex-row' >
                 {/* Create a component for listing out the projects */}
-                <div className='flex flex-col cursor-pointer' >
+                <div className='projects-sec-cards-panel flex flex-col cursor-pointer' >
                     {renderProjectsList}
                 </div>
                 {/* Create a component for rendering a a function as recieved in a object */}
-                <div className='border w-full'>
+                <div className=' w-full'>
                     <div className='flex flex-row items-center justify-between' >
                         <h2 className='text-2xl font-bold px-10 pt-10 pb-3'>{projectsData[activeProject].title}</h2>
                         <div className='px-10 pt-10 pb-3 cursor-pointer hover:text-green-400 transition-0p1' >
