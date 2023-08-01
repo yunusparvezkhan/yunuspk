b56a093881fda0f69e39dbf53684cc5c7c864f63
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-02 04:06:19 +0530

    feat(UnderDevNotifier): include a external animation

--- src/components/UnderDevNotifier.js
+++ src/components/UnderDevNotifier.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const UnderDevNotifier = () => {
+const UnderDevNotifier = ({ animationHeight }) => {
     return (
-        <div>
-            <center>
+        <div className='h-full w-full absolute' >
+            <div className='absolute t-40percent left-1/2 ttff ' >
+                <center>
+                    <iframe src='https://lottie.host/?file=30586555-da89-41aa-83ba-e1e8d92784fc/mXyBOCCNhC.json' title='development-animation' height={animationHeight} />
+                </center>
                 <h1 className='text-4xl'>
                     This page is under development!
                 </h1>
-            </center>
-        </div>
+            </div>
+        </div >
     )
 }
 
--- src/pages/AboutPage/index.js
+++ src/pages/AboutPage/index.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
     return (
         <div>
             AboutPage
-            <UnderDevNotifier />
+            <UnderDevNotifier animationHeight="300px" />
         </div>
     )
 }
