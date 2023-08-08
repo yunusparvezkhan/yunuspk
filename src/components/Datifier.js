76b6d89ff0e9ee9d378709cbbd2258f3fe05a4a3
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-08 23:12:44 +0530

    feat(Datifier): create machanism for day suffix in dmy-b type

--- src/components/Datifier.js
+++ src/components/Datifier.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react'
 
 const Datifier = ({ type, day, month, year }) => {
-    return (
-        <div>
-            This component will take date organising type, day, month and year as a prop and will turn that into a string of the desired type
-        </div>
-    )
+
+    let daySuffix;
+    const dayString = String(day)
+
+    if (dayString.length === 2) {
+        if (dayString[0] !== "1") {
+            if (dayString[1] === "1") {
+                daySuffix = "st";
+            } else if (dayString[1] === "2") {
+                daySuffix = "nd";
+            } else if (dayString[1] === "3") {
+                daySuffix = "rd";
+            } else {
+                daySuffix = "th";
+            }
+        } else if (dayString[0] === "1") {
+            daySuffix = "th";
+        }
+    } else if (dayString.length === 1) {
+        if (dayString[0] === "1") {
+            daySuffix = "st";
+        } else if (dayString[0] === "2") {
+            daySuffix = "nd";
+        } else if (dayString[0] === "3") {
+            daySuffix = "rd";
+        } else {
+            daySuffix = "th";
+        }
+    }
+
+    if (type === "dmy-b") {
+        return <div>{dayString + daySuffix}</div>
+    }
 }
 
 export default Datifier
