1517dda99da963c37136323c2984f9cb5c9bc902
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-08 23:26:26 +0530

    feat(Datifier): add comments about how to use Datifier types

--- src/components/Datifier.js
+++ src/components/Datifier.js
@@ -1,4 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
+
+
+// This component will have essentially have 12 types which are planned to be created in next commits.
+
+// type writting format :-  xyz-p, where x,y,z(aka format) are either d,m,y (meaning day, month, year respectively) (the name should have either dmy or mdy) 
+// and p (aka structure) is some alphabet between a to f from below table
+
+// structure    |   format dmy               |   format mdy
+// ------------------------------------------------------------------------------------------------------
+//      A       |   Seventh of August, 2023  |   August the Seventh, 2023                               |
+//      B       |   7th August, 2023         |   August 7th, 2023                                       | 
+//      C       |   7 August 2023            |   August 7, 2023                                         |
+//      D       |   7/8/2023                 |   8/7/2023                                               |
+//      E       |   7/8/23                   |   8/7/23                                                 |   
+//      F       |   07/08/23                 |   08/07/23                                               |
+// ------------------------------------------------------------------------------------------------------
+
+
 
 const Datifier = ({ type, day, month, year }) => {
 
