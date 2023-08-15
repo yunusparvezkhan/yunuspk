f4eb72231bca3fe3cbe83e306a04b2eea1638982
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-15 23:45:14 +0530

    cleanup(landingpage/index): remove unused imports

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import IntroSection from './sections/IntroSection';
-import AboutSection from './sections/AboutSection';
 import './styles/landingpage.css'
-import BlogsSection from './sections/BlogsSection';
-import ProjectsSection from './sections/ProjectsSection';
 import IntroWidget from '../../components/widgets/IntroWidget';
 import AboutWidget from '../../components/widgets/AboutWidget';
 import BlogsWidget from '../../components/widgets/BlogsWidget';
