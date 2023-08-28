6d56b7034e8e3333aad7a8a1930104cce704fa92
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-28 14:27:46 +0530

    feat(project panels): pretify panel comps

--- src/widgets/projects/panels/Picsfetapp.js
+++ src/widgets/projects/panels/Picsfetapp.js
@@ -17,29 +17,42 @@ const Picsfetapp = () => {
             </div>
             <h1 className='text-2xl mb-2' >Introduction</h1>
             <p className='text-md text-gray-300 mb-8'>
-                pics-fetching-app, (aka chitro) is an app that fetches Images matching with the user’s prompted query. This app uses api from unsplash & renders upto 10 available images to user.
+                pics-fetching-app, (aka chitro) is an app that fetches Images matching
+                with the user’s prompted query. This app uses api from unsplash & renders
+                upto 10 available images to user.
             </p>
 
             <h1 className='text-2xl mb-2' >Purpose</h1>
             <p className='text-md text-gray-300 mb-8'>
-                The purpose of building Chitro is to understand the concepts of fetching data from an api. This app also teaches and practices the state management concepts.
+                The purpose of building Chitro is to understand the concepts of fetching
+                data from an api. This app also teaches and practices the state management
+                concepts.
             </p>
 
             <h1 className='text-2xl mb-2' >Overview</h1>
             <p className='text-md text-gray-300 mb-8'>
-                This app essentially has 2 sections. Those are the Search Feature & the Search Result Feature. The 2nd feature, Search Results, would not render at the first load of the application. It is when the user types some keywords and hits enter, the app sends request to the api & gets back some results. The app then renders the Search Results as images in a responsive grid UI.
+                This app essentially has 2 sections. Those are the Search Feature & the
+                Search Result Feature. The 2nd feature, Search Results, would not render
+                at the first load of the application. It is when the user types some keywords
+                and hits enter, the app sends request to the api & gets back some results.
+                The app then renders the Search Results as images in a responsive grid UI.
             </p>
 
             <h1 className='text-2xl mb-2' >Features</h1>
             <div className='mb-8 ml-5'>
                 <h2 className='text-lg font-semibold' >1. Search Feature</h2>
                 <p className='text-md text-gray-300 mb-3 ml-5'>
-                    Search Feature renders at the very top of the app. This is basically a search input bar and a submit button with a search icon. Users initially can type their queries here and hit the submit button or enter on keyboard.
+                    Search Feature renders at the very top of the app. This is basically
+                    a search input bar and a submit button with a search icon. Users
+                    initially can type their queries here and hit the submit button or
+                    enter on keyboard.
                 </p>
 
                 <h2 className='text-lg font-semibold' >2. Search Results Feature</h2>
                 <p className='text-md text-gray-300 mb-3 ml-5'>
-                    After user submits some queries, the app sends a request to Unsplash with the user’s queries, and prints the images got back from the api as a grid view below the search feature.
+                    After user submits some queries, the app sends a request to Unsplash
+                    with the user’s queries, and prints the images got back from the api
+                    as a grid view below the search feature.
                 </p>
 
             </div>
--- src/widgets/projects/panels/Ulbm.js
+++ src/widgets/projects/panels/Ulbm.js
@@ -16,17 +16,25 @@ const Ulbm = () => {
             </div>
             <h1 className='text-2xl mb-2' >Introduction</h1>
             <p className='text-md text-gray-300 mb-8'>
-                Ulbumy is a app that organises user datasets in a three layer level. Initially this app has a button to create a user, then inside the user tabs, it has option to create albums and inside albums it has features to create images. This way albums of different photos under different users can be organised.
+                Ulbumy is a app that organises user datasets in a three layer level.
+                Initially this app has a button to create a user, then inside the user tabs,
+                it has option to create albums and inside albums it has features to create images.
+                This way albums of different photos under different users can be organised.
             </p>
 
             <h1 className='text-2xl mb-2' >Purpose</h1>
             <p className='text-md text-gray-300 mb-8'>
-                Ulbumy is a beginner project in react js to practice and learn about async thunks and backend data fetching methods with reduxjs store.
+                Ulbumy is a beginner project in react js to practice and learn about async
+                thunks and backend data fetching methods with reduxjs store.
             </p>
 
             <h1 className='text-2xl mb-2' >Overview</h1>
             <p className='text-md text-gray-300 mb-8'>
-                This app on the top level has 3 features, that’s users, albums & images. At the landing in the app, it has feature to add a new user, and inside every user’s panel (that will apprear as a list), there is a feature to add a new album, and exactly in the same way, every album has a feature to add images.
+                This app on the top level has 3 features, that’s users, albums & images.
+                At the landing in the app, it has feature to add a new user, and inside
+                every user’s panel (that will apprear as a list), there is a feature to
+                add a new album, and exactly in the same way, every album has a feature
+                to add images.
             </p>
 
 
@@ -39,17 +47,21 @@ const Ulbm = () => {
             <div className='mb-8 ml-5'>
                 <h2 className='text-lg font-semibold' >1. User's Feature</h2>
                 <p className='text-md text-gray-300 mb-3 ml-5'>
-                    The search feature in Ulbumy creates a user, and sends that request to backend as well. Inside every user, multiple albums can be created.
+                    The search feature in Ulbumy creates a user, and sends that request
+                    to backend as well. Inside every user, multiple albums can be created.
                 </p>
 
                 <h2 className='text-lg font-semibold' >2. Albums Feature</h2>
                 <p className='text-md text-gray-300 mb-3 ml-5'>
-                    The albums feature in Ulbumy creates an album under a certain user. This feature also sends that data/request to the backend as well.
+                    The albums feature in Ulbumy creates an album under a certain user.
+                    This feature also sends that data/request to the backend as well.
                 </p>
 
                 <h2 className='text-lg font-semibold' >3. Images Feature</h2>
                 <p className='text-md text-gray-300 mb-3 ml-5'>
-                    The images feature in Ulbumy creates a random image under an album using the faker js library. This feature too, send that data to the backend.
+                    The images feature in Ulbumy creates a random image under an album
+                    using the faker js library. This feature too, send that data to the
+                    backend.
                 </p>
 
             </div>
