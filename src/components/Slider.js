26883e886387889f47fafbccaa0f96a558515cdc
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-08-30 00:01:43 +0530

    feat(slider): create rotational array of 5 id(s)

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Slider = () => {
+
+    const slides = [
+        {
+            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            title: "some certificate",
+            description: "some description"
+        },
+        {
+            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            title: "some certificate",
+            description: "some description"
+        },
+        {
+            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            title: "some certificate",
+            description: "some description"
+        },
+        {
+            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            title: "some certificate",
+            description: "some description"
+        },
+        {
+            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            title: "some certificate",
+            description: "some description"
+        }
+    ]
+
+    const [orientation, setOrientation] = useState(["alpha", "beta", "gamma", "delta", "epsilon"])
+
+    const onNext = async () => {
+        const NewOrientation = orientation.slice(1)
+        NewOrientation.push(orientation[0]);
+        console.log(NewOrientation);
+        setOrientation(NewOrientation);
+    }
+
     return (
-        <div>Slider</div>
+        <div>
+            Slider
+            <button onClick={onNext}>Next</button>
+        </div>
     )
 }
 
-export default Slider
+export default Slider;
