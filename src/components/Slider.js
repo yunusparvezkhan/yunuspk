2cdd850630374e64e9b65e3631876ac36ea351a2
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-09-02 21:50:07 +0530

    feat(achievements): add real certificate image links

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,41 +7,53 @@ const Slider = () => {
 
     const slides = [
         {
-            image: "https://marketplace.canva.com/EAFNlUJs5g4/2/0/1600w/canva-white-simple-certificate-of-appreciation-Fcz7KkZ5YaU.jpg",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-1.jpg",
             title: "some certificate",
             codeName: "alpha",
             description: "some description"
         },
         {
-            image: "https://marketplace.canva.com/EAFHBiEZGD8/1/0/1600w/canva-red-and-yellow-minimalist-employee-of-the-month-certificate-67ATPnoOO0M.jpg",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-2.jpg",
             title: "some certificate",
             codeName: "beta",
             description: "some description"
         },
         {
-            image: "https://img.freepik.com/free-vector/flat-certificate-template_52683-61537.jpg?w=2000",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-3.jpg",
             title: "some certificate",
             codeName: "gamma",
             description: "some description"
         },
         {
-            image: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/certificate-of-appreciation-design-template-7289b7fef37b1bda2dc3527df90bfe87_screen.jpg?ts=1631442106",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-4.jpg",
             title: "some certificate",
             codeName: "delta",
             description: "some description"
         },
         {
-            image: "https://cdn.cp.adobe.io/content/2/rendition/21d3dc6d-9d20-4833-bb77-6b7e80e5f46b/artwork/c4040e75-5023-4d2f-9037-3a1b9b487988/version/0/format/jpg/dimension/width/size/400",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-5.jpg",
             title: "some certificate",
             codeName: "epsilon",
             description: "some description"
         },
         {
-            image: "https://cdn.cp.adobe.io/content/2/rendition/21d3dc6d-9d20-4833-bb77-6b7e80e5f46b/artwork/c4040e75-5023-4d2f-9037-3a1b9b487988/version/0/format/jpg/dimension/width/size/400",
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-6.jpg",
             title: "some certificate",
             codeName: "zeta",
             description: "some description"
         },
+        {
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-7.jpg",
+            title: "some certificate",
+            codeName: "eta",
+            description: "some description"
+        },
+        {
+            image: "https://mediabase.netlify.app/achievements/certificates/compressed/670px/certificate-8.jpg",
+            title: "some certificate",
+            codeName: "theta",
+            description: "some description"
+        }
     ]
 
     const [orientation, setOrientation] = useState(slides.map((slide) => slide));
