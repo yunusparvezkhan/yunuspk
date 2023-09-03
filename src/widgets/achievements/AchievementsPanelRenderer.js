5b26bfe1e78f9e44af9be437cd7b99017e7a0038
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-09-03 21:13:46 +0530

    feat(achievementsWidget): render description in paragraphs

--- src/widgets/achievements/AchievementsPanelRenderer.js
+++ src/widgets/achievements/AchievementsPanelRenderer.js
@@ -2,6 +2,15 @@ import React from 'react';
 import "./achievementsPanel.css"
 
 const AchievementsPanelRenderer = ({ title, description, codeName }) => {
+
+    const renderDescription = description.map((paragraph) => {
+        return (
+            <div className='mb-3'>
+                {paragraph}
+            </div>
+        )
+    })
+
     return (
         <div className='mb-96 achievementsPanel' >
             <div className='achievements-panel-title-bar w-contain mx-10 flex flex-row items-center' >
@@ -10,9 +19,9 @@ const AchievementsPanelRenderer = ({ title, description, codeName }) => {
                 </h1>
             </div>
             <div className='achievements-panel-description-box px-10' >
-                <p >
-                    {description}
-                </p>
+                <div className='achievements-panel-description-box-text'>
+                    {renderDescription}
+                </div>
             </div>
         </div>
     )
